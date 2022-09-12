269d3b979e9495ab0c86ae47f121253bfd7f26b7
a01658142 <a01658142@itesm.mx>
2022-09-12 16:48:54 -0500

    gif jack y rose, nombres y matriculas

--- screens/FirstScreen.js
+++ screens/FirstScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import {useNavigation} from '@react-navigation/native'
+import * as Animatable from 'react-native-animatable';
 
 export default function FirstScreen() {
 
@@ -13,12 +14,18 @@ useLayoutEffect(() => {
 
   return (
     <SafeAreaView className='pt-10 items-center'>
-      <View>
-        <Text>FirstScreen</Text>
-        <Text>imagen barco hundiéndose</Text>
+      <View className='flex-1'>
+        <Text>¿Sobrevivirías al Titanic?</Text>
+        <Animatable.Image
+          source={require('../assets/jackdrowning.gif')}
+          animation='slideInUp'
+          iterationCount={1}
+          className='h-85 w-85'
+        />
+        <Text>En "Titanic Jack or Rose App" vas a saber ¿quién de los dos serías?</Text>
       </View>
       <View>
-        <Text>Equipo Random</Text>
+        <Text>Equipo 1</Text>
       </View>
       <TouchableOpacity onPress={() => navigation.navigate('Home')} className='w-20 h-20 rounded bg-red-600'>
         <Text className='text-base text-white'>Start</Text>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,11 +10,11 @@ export default function HomeScreen() {
   const actions = [
     {
       text: "Prediction",
-      icon: <GlobeAltIcon size={30} color='white'
+      icon: <GlobeAltIcon size={30} color='#fff'
       onPress={()=>navigation.navigate('PredictionScreen')}/>,
       name: "bt_alergy",
       position: 1,
-      color: '#ffb451'
+      color: '#8700CB'
     },
     {
       text: "Algoritmo 1",
@@ -22,7 +22,7 @@ export default function HomeScreen() {
       onPress={()=>navigation.navigate('Screen_Alg1')}/>,
       name: "bt_language",
       position: 3,
-      color: '#ffb451'
+      color: '#8700CB'
     },
     {
       text: "Algoritmo 2",
@@ -30,7 +30,7 @@ export default function HomeScreen() {
       onPress={()=>navigation.navigate('Screen_Alg2')}/>,
       name: "bt_room",
       position: 4,
-      color: '#ffb451'
+      color: '#8700CB'
     },
     {
       text: "Selection",
@@ -38,18 +38,8 @@ export default function HomeScreen() {
       onPress={()=>navigation.navigate('SelectionScreen')}/>,
       name: "bt_videocam",
       position: 2,
-      color: '#ffb451'
+      color: '#8700CB'
     }
-    // {text: 'Prediction',
-    // icon: <ShoppingBagIcon size={30} color='red' />,
-    // name: 'pred',
-    // position: 3,
-    // color: '#fff'},
-    // {text: 'Prediction',
-    // icon: <GlobeAltIcon size={30} color='red' />,
-    // name: 'pred',
-    // position: 4,
-    // color: '#fff',},
   ]
 
   useLayoutEffect(() => {
@@ -60,36 +50,36 @@ export default function HomeScreen() {
 
 
   return (
-    <SafeAreaView className='bg-red-500 pt-10'>
-      <ScrollView className='bg-[#fdfbf0] flex-1 ' 
+    <SafeAreaView className='bg-white pt-10'>
+      <ScrollView className='bg-black flex-1 ' 
         contentContainerStyle={{
           paddingBottom:350,
         }}>
-        <View>
-          <Text>Nombre del reto</Text>
-          <Text>Descripción del problema</Text>
-          <Text>Importancia de resolver el problema</Text>
+        <View className='p-2 m-4 '>
+          <Text className='mb-3 mx-8 text-2xl text-white text-center'>Competencia Titanic Machine Learning</Text>
+          <Text className='mb-3 text-sm text-justify text-fuchsia-200'>Usar Machine Learning para encontrar un modelo predictivo que arroje cuáles pasajeros pueden sobrevivir al naufragio del Titanic. El hundimiento del famoso Titanic fue uno de los naufragios más crueles de la historia, el día 15 de abril de 1912, en el transcurso de su primer viaje, el transatlántico que era considerado por su envergadura como insumergible, fue hundido al chocar contra un iceberg. Dado que no se pensaba que fueran a necesitar los botes salvavidas, no hubo suficientes para todos los pasajeros. Aquello resultó en una gran tragedia con 1502 muertos de los 224 pasajeros y tripulantes. Algunas personas o grupos tuvieron mayor probabilidad de sobrevivir, gracias a ciertas condiciones que vamos a predecir construyendo dos modelos predictivos. ¿Qué tipo de pasajero tendría mas probabilidad de sobrevivir?, de acuerdo a datos como: Nombre, Edad, Sexo, Clase socioeconómica, entre otras.</Text>
+          <Text className='text-sm text-justify text-fuchsia-200'>Es importante lograr un porcentaje alto de predicción, ya que, este tipo de eventos ocurre no solo con barcos, también, en aviones, trenes, naves espaciales, entre otros transportes. Además, puede ayudar a mejorar la seguridad en otros casos como terremotos, huracanes, deslaves y muchos otros desastres naturales. Aunque, los parámetros pueden cambiar, los modelos suelen ser parecidos o al menos es una forma de acercamiento a la resolución del problema.</Text>
         </View>
-        <View>
-          <Text>Equipo 1</Text>
-          <View>
-            <Text>Alumno 1</Text>
-            <Text>matricula 1</Text>
+        <View className='m-6'>
+          <Text className='mb-3 mx-8 text-xl text-white text-center'>Equipo 1</Text>
+          <View className='flex-row'>
+            <Text className='mb-3 text-sm text-fuchsia-200'>Jorge Gerardo Iglesias Ortiz</Text>
+            <Text className='mb-3 mx-2 text-sm  text-fuchsia-200'>A01653261</Text>
           </View>
-          <View>
-            <Text>Alumno 2</Text>
-            <Text>matricula 2</Text>
+          <View className='flex-row'>
+            <Text className='mb-3 text-sm text-fuchsia-200'>Francisco Emiliano Rocha Pineda</Text>
+            <Text className='mb-3 mx-2 text-sm text-fuchsia-200'>A01652082</Text>
           </View>
-          <View>
-            <Text>Alumno 3</Text>
-            <Text>matricula 3</Text>
+          <View className='flex-row'>
+            <Text className='mb-3 text-sm text-fuchsia-200'>Jorge Antonio Hoyo García</Text>
+            <Text className='mb-3 mx-2 text-sm text-fuchsia-200'>A01658142</Text>
           </View>
         </View>
         {/*FloatIcon */}
-        <View className='absolute pt-[350px] pl-[380px]'>
+        <View className='absolute pt-[750px] pl-[380px]'>
           <FloatingAction
             actions= {actions}
-            color='#ffb451'
+            color='#8700CB'
           /> 
         </View>
       </ScrollView>
