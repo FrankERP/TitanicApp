9b4bae50752fa19a3bcf271fd347923a9e35ebfe
Francisco Rocha <frankrochapineda@hotmail.com>
2022-09-12 16:41:28 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import {useNavigation} from '@react-navigation/native'
 
@@ -12,10 +12,16 @@ export default function HomeScreen() {
   }, [])
 
   return (
-    <View>
+    <SafeAreaView>
+      <View>
+        <Text className='font-extrabold text-gray-300'>
+          Header 
+        </Text>
+      </View>
       <Text>HomeScreen</Text>
       <Text>Texto para probar commit</Text>
       <Text>Texto para probar commit Jorge</Text>
-    </View>
+    
+    </SafeAreaView>
   )
 }
