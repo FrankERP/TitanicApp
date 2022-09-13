cd2696f07ab8e2db350be744efe68039211073e0
Francisco Rocha <frankrochapineda@hotmail.com>
2022-09-12 19:23:52 -0500

    Prediction correct format

--- screens/PredictionScreen.js
+++ screens/PredictionScreen.js
@@ -1,68 +1,93 @@
-import { View, Text, Button, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, Button, SafeAreaView, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import {TextInput} from 'react-native-paper'
 import { Formik } from 'formik'
 import axios from 'axios'
 import { useState } from 'react'
+import { ArrowLeftIcon } from 'react-native-heroicons/solid'
+import { useNavigation } from '@react-navigation/native'
 
 export default function PredictionScreen() {
 
   const [prediction, setPrediction] = useState('')
+  const navigation = useNavigation();
+
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [])
 
   return (
-    <SafeAreaView>
-    <Formik initialValues = {{
-      sepalLength: '',
-      sepalWidth:'',
-      petalLength: '',
-      petalWidth: ''
-      }}
-      //onSubmit={values => console.log(values)}
-      onSubmit={values => 
-        axios.post('http://10.48.187.54:5000/predict',{
-          sepalLength: values.sepalLength,
-          sepalWidth: values.sepalWidth,
-          petalLength: values.petalLength,
-          petalWidth: values.petalWidth,
-        }).then(response => {setPrediction(response.data)}).catch(error => {console.log(error)})}
-
-    >
-      {({handleChange,handleBlur, handleSubmit, values})=> (
-        <View>
-          <TextInput className='bg-gray-300'
-            onChangeText = {handleChange('sepalLength')}
-            onBlur={handleBlur('sepalLength')}
-            vaue = {values.sepalLength}
-            placeholder = 'sepalLength'
-            placeholderTextColor={'#AF00CC'}
-          />
-          <TextInput className='bg-gray-400'
-            onChangeText = {handleChange('sepalWidth')}
-            onBlur={handleBlur('sepalWidth')}
-            vaue = {values.sepalWidth}
-            placeholder = 'sepalWidth'
-            placeholderTextColor={'#AF00CC'}
-          />
-          <TextInput className='bg-gray-300'
-            onChangeText = {handleChange('petalLength')}
-            onBlur={handleBlur('petalLength')}
-            vaue = {values.petalLength}
-            placeholder = 'petalLength'
-            placeholderTextColor={'#AF00CC'}
-          />
-          <TextInput className='bg-gray-400'
-            onChangeText = {handleChange('petalWidth')}
-            onBlur={handleBlur('petalWidth')}
-            vaue = {values.petalWidth}
-            placeholder = 'petalWidth'
-            placeholderTextColor={'#AF00CC'}
-          />
-          <Button onPress={handleSubmit} title='Submit'/>
-
-          <Text className='text-xl text-indigo-600'>The Prediction is: {prediction}</Text>
+    <SafeAreaView className='bg-gray-900 flex-1'>
+      <View>
+        <View className='p-7 border-b border-[#AF00CC] bg-gray-900 shadow-xs'>
+          <View>
+            <Text className='text-lg font-bold text-center text-gray-50'>Prediction Screen</Text>
+          </View>
+          <TouchableOpacity onPress={navigation.goBack} 
+            className='absolute top-6 left-5 p-2 bg-gray-900 border border-solid border-purple-600 rounded-full'>
+              <ArrowLeftIcon size={20} color='#AF00CC'/>
+          </TouchableOpacity>
         </View>
-      ) }
-    </Formik>
+      </View>
+      <Formik initialValues = {{
+        sepalLength: '',
+        sepalWidth:'',
+        petalLength: '',
+        petalWidth: ''
+        }}
+        //onSubmit={values => console.log(values)}
+        onSubmit={values => 
+          axios.post('http://10.48.187.54:5000/predict',{
+            sepalLength: values.sepalLength,
+            sepalWidth: values.sepalWidth,
+            petalLength: values.petalLength,
+            petalWidth: values.petalWidth,
+          }).then(response => {setPrediction(response.data)}).catch(error => {console.log(error)})}
+
+      >
+        {({handleChange,handleBlur, handleSubmit, values})=> (
+          <View>
+            <TextInput className='bg-gray-800'
+              onChangeText = {handleChange('sepalLength')}
+              onBlur={handleBlur('sepalLength')}
+              vaue = {values.sepalLength}
+              placeholder = 'sepalLength'
+              placeholderTextColor={'#AF00CC'}
+              theme={{colors:{text:'#E381C7'}}}
+            />
+            <TextInput className='bg-gray-700'
+              onChangeText = {handleChange('sepalWidth')}
+              onBlur={handleBlur('sepalWidth')}
+              vaue = {values.sepalWidth}
+              placeholder = 'sepalWidth'
+              placeholderTextColor={'#AF00CC'}
+              theme={{colors:{text:'#E381C7'}}}
+            />
+            <TextInput className='bg-gray-800'
+              onChangeText = {handleChange('petalLength')}
+              onBlur={handleBlur('petalLength')}
+              vaue = {values.petalLength}
+              placeholder = 'petalLength'
+              placeholderTextColor={'#AF00CC'}
+              theme={{colors:{text:'#E381C7'}}}
+            />
+            <TextInput className='bg-gray-700 text-fuchsia-200'
+              onChangeText = {handleChange('petalWidth')}
+              onBlur={handleBlur('petalWidth')}
+              vaue = {values.petalWidth}
+              placeholder = 'petalWidth'
+              placeholderTextColor={'#AF00CC'}
+              theme={{colors:{text:'#E381C7'}}}
+            />
+            <Button onPress={handleSubmit} title='Submit' color={'#AF00CC'}/>
+
+            <Text className='text-xl text-fuchsia-200'>The Prediction is: {prediction}</Text>
+          </View>
+        ) }
+      </Formik>
     </SafeAreaView>
   )
 }
